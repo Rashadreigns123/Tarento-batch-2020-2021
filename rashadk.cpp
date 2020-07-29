bd70d92951f46df12aa632b35c00e4ac8c707a69
rashad <rashadreigns@gmail.com>
2020-07-29 12:43:29 +0530

    My commit

---
+++ rashadk.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int n1,n2,sum=0;
+    cout<<"Enter first number: ";
+    cin>>n1;
+    cout<<"Enter second number: ";
+    cin>>n2;
+    sum=n1+n2;
+    cout<<"Sum of "<<n1<<" and "<<n2<<" is "<<sum;
+    
+    return 1;
+    
+}    
+    
+    
+    
